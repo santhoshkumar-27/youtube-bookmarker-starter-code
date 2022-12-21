49036bdb7ed34399e9dd44c277a1ba8b7f548174
Jayasurya Ravichandran <jayasurya@twilightsoftwares.com>
2022-12-21 10:46:58 +0530

    feat: initial commit

---
+++ popup.js
@@ -0,0 +1,16 @@
+// adding a new bookmark row to the popup
+const addNewBookmark = () => { };
+
+const viewBookmarks = () => { };
+
+const onPlay = e => { };
+
+const onDelete = e => { };
+
+const setBookmarkAttributes = () => { };
+
+document.addEventListener("DOMContentLoaded", (event) => { 
+    console.log('browser extenison loaded')
+    const bookmark = document.getElementById('bookmarks');
+    bookmark.innerText = 'Welcome to the my youtube bookmark';
+});
